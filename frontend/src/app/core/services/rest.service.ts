b1e27f772adc0b28b33e6f7d04e204c46c184d2e
Adrian Kulik <adriankulik93@gmail.com>
2025-10-26 19:24:52 +0100

    Call backend

--- frontend/src/app/core/services/rest.service.ts
+++ frontend/src/app/core/services/rest.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, from } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ApiEndpoints } from '../constants/api-endpoints';
 
 @Injectable({
@@ -11,35 +11,19 @@ export class RestService {
 
   constructor(private http: HttpClient) {}
 
-  private isElectron(): boolean {
-    return !!(window as any).electronAPI;
-  }
-
   get<T>(url: string): Observable<T> {
-    if (this.isElectron()) {
-      return from((window as any).electronAPI.apiRequest(url, 'GET')) as Observable<T>;
-    }
     return this.http.get<T>(url);
   }
 
   post<T>(url: string, data: any): Observable<T> {
-    if (this.isElectron()) {
-      return from((window as any).electronAPI.apiRequest(url, 'POST', data)) as Observable<T>;
-    }
     return this.http.post<T>(url, data);
   }
 
   put<T>(url: string, data: any): Observable<T> {
-    if (this.isElectron()) {
-      return from((window as any).electronAPI.apiRequest(url, 'PUT', data)) as Observable<T>;
-    }
     return this.http.put<T>(url, data);
   }
 
   delete<T>(url: string): Observable<T> {
-    if (this.isElectron()) {
-      return from((window as any).electronAPI.apiRequest(url, 'DELETE')) as Observable<T>;
-    }
     return this.http.delete<T>(url);
   }
 }
