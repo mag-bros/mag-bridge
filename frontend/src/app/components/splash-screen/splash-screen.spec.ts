9a289bf7e31b2c3ed55bb24c2bc8bc129975d0ce
Adrian Kulik <adriankulik93@gmail.com>
2025-10-28 20:35:23 +0100

    [feat] add splash-screen component

---
+++ frontend/src/app/components/splash-screen/splash-screen.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SplashScreen } from './splash-screen';
+
+describe('Loader', () => {
+  let component: SplashScreen;
+  let fixture: ComponentFixture<SplashScreen>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SplashScreen],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SplashScreen);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
