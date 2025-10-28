49f7c6d03645387ce9d348589824adc8d79426da
Adrian Kulik <adriankulik93@gmail.com>
2025-10-28 20:35:38 +0100

    [feat] use new component in app root

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,11 +2,34 @@ import { Component } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
 import { Form } from './components/form/form';
+import { SplashScreen } from './components/splash-screen/splash-screen';
+import { RestService } from './core/services/rest.service';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatSlideToggleModule, MatButtonModule, Form],
+  imports: [MatSlideToggleModule, MatButtonModule, Form, SplashScreen],
   templateUrl: './app.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private restService: RestService) {}
+  isLoading = true;
+  response: string | undefined;
+  backendCheckSubscription!: Subscription;
+
+  ngOnInit() {
+    this.backendReadyCheck();
+  }
+
+  backendReadyCheck() {
+    this.backendCheckSubscription = interval(500).subscribe(() => {
+      this.restService.get(this.restService.endpoints.general.home).subscribe({
+        next: () => {
+          this.isLoading = false;
+          this.backendCheckSubscription.unsubscribe();
+        },
+      });
+    });
+  }
+}
