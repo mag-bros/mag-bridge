70349e5a08014d048b8c17669400fcc21935a9cd
kpwydra <kpwydra@gmail.com>
2025-11-05 15:45:54 +0100

    added tests - all passed

--- tests/unit/test_data.py
+++ tests/unit/test_data.py
@@ -9,6 +9,10 @@ SDF_TEST_COMPOUNDS = [
         "expected_diamag": -788.64,
         "description": "RDKit is treating all C and N atoms as ring_atoms when molecule has macrocyclic structure.",
     },
+    {"sdf_file": "[Ag(TACN)](HSO4).sdf", "expected_diamag": -160.99},
+    {"sdf_file": "joint-ring-system.sdf", "expected_diamag": -169.88},
+    {"sdf_file": "azabicycle_9_5.sdf", "expected_diamag": -116.92},
+    {"sdf_file": "macrocycle_with_rings.sdf", "expected_diamag": -519.24},
     # {"sdf_file": "Be(CH3)2.sdf", "expected_diamag": -29.98},
     # TODO: This test will not pass because Be-C bonds have to be omitted. To resolve it, error for user must be provided
     #       or additional function that delates certain coordination bonds and updates raw SDF file.#
