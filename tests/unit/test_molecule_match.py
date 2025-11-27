60c718f074e8b661bf25dd708c7c6d9f96e7a6af
kpwydra <kpwydra@gmail.com>
2025-11-27 13:27:47 +0100

    added test for common molecule matching

--- tests/unit/test_molecule_match.py
+++ tests/unit/test_molecule_match.py
@@ -5,27 +5,42 @@ import pytest
 from rdkit import Chem
 
 from loader import SDFLoader
+from src import SDF_TEST_DIR
 from src.core.compound import MBCompound
 
 
-@pytest.mark.parametrize("test_case", SDF_TEST_COMPOUNDS)
-def test_calc_diamag_sdf_compounds(test_case: dict[str, Any]) -> None:
+def test_molecule_match() -> None:
     """Loop through SDF test cases files"""
-    # Load all MBMolecule instances from the given SDF file
-    compound: MBCompound = SDFLoader.Load(test_case)
 
-    # Calc Diamag Contr of a Compound defined by a single SDF file
-    diamag_contr: float = compound.CalcDiamagContr()
+    # Load all MBCompound instances from the given SDF file
+    sdf_files = [p.name for p in SDF_TEST_DIR.glob("*.sdf")]
+    compounds = [SDFLoader.Load(filename, "tests") for filename in sdf_files]
 
-    # Test Result
-    try:
-        # trunk-ignore(bandit/B101)
-        assert round(diamag_contr, 2) == test_case["expected_diamag"]
-        print(
-            f'[INF] "{compound.source_file}": ✅ Diamag is as expected: {diamag_contr:.4f}'
-        )
-    except AssertionError as e:
-        print(
-            f'[ERR] "{compound.source_file}": ❌ result {round(diamag_contr, 2)} is not expected value: {test_case["expected_diamag"]}'
-        )
-        raise e
+    EXCEPTION = ["S2O32-.sdf"]
+
+    for compound in compounds:
+        group = []
+        for mol in compound.GetMols(to_rdkit=False):
+            group.append(mol.ToSmiles())
+
+        # Test Results
+        try:
+            # trunk-ignore(bandit/B101)
+            # Exception (thiosulfate anion)
+            if compound.source_file in EXCEPTION:
+                print(
+                    f'[INF] "{compound.source_file}": ⚠ Allowed {set(group)} canonical SMILES case'
+                )
+                continue
+
+            # Standard assertion
+            else:
+                assert len(set(group)) == 1
+                print(
+                    f'[INF] "{compound.source_file}": ✅ One canonical SMILES created: {set(group)}'
+                )
+        except AssertionError as e:
+            print(
+                f'[ERR] "{compound.source_file}": ❌ {len(set(group))} canonical SMILES generated: {set(group)}'
+            )
+            raise e
