f3d0bdd8caad87183fd3848ed7d7dd32516b2731
rimon <34632434+Sevelantis@users.noreply.github.com>
2025-11-30 19:49:05 +0100

    [core] descriptor classes for constants, constants cleanup (#77)

--- tests/core/test_diamag.py
+++ tests/core/test_diamag.py
@@ -1,24 +1,29 @@
-from typing import Any
-
 import pytest
 
 from src import DIAMAG_COMPOUND_ATOMS_SUBDIR
 from src.core.compound import MBCompound
 from src.loader import SDFLoader
-from tests.integration.test_data import SDF_TEST_COMPOUNDS
+from tests.core.test_data import SDF_TEST_COMPOUNDS, SDFTestCompound
 
 
-@pytest.mark.parametrize("test_case", SDF_TEST_COMPOUNDS)
-def test_calc_diamag_sdf_compounds(test_case: dict[str, Any]) -> None:
+@pytest.mark.parametrize(
+    "test_compound_params",
+    list(enumerate(SDF_TEST_COMPOUNDS)),
+    ids=lambda p: f"<test:{p[0]}> {p[1].sdf_file}",
+)
+def test_calc_diamag_sdf_compounds(
+    test_compound_params: tuple[int, SDFTestCompound],
+) -> None:
     """Internal Integration Test testing the whole diamag calcs process.
     Logical components:
         1. Load SDF
         2. Calc Diamag Contr
         3. Compare calculated result to expected
     """
+    idx, sdf_test_compound = test_compound_params
     # Load all MBMolecule instances from the given SDF file
     compound: MBCompound = SDFLoader.Load(
-        test_case["sdf_file"], subdir=DIAMAG_COMPOUND_ATOMS_SUBDIR
+        sdf_test_compound.sdf_file, subdir=DIAMAG_COMPOUND_ATOMS_SUBDIR
     )
 
     # Calc Diamag Contr of a Compound defined by a single SDF file
@@ -26,13 +31,12 @@ def test_calc_diamag_sdf_compounds(test_case: dict[str, Any]) -> None:
 
     # Test Result
     try:
-        # trunk-ignore(bandit/B101)
-        assert round(diamag_contr, 2) == test_case["expected_diamag"]
+        assert round(diamag_contr, 2) == sdf_test_compound.expected_diamag_total
         print(
             f'[INF] "{compound.source_file}": ✅ Diamag is as expected: {diamag_contr:.4f}'
         )
     except AssertionError as e:
         print(
-            f'[ERR] "{compound.source_file}": ❌ result {round(diamag_contr, 2)} is not expected value: {test_case["expected_diamag"]}'
+            f'[ERR] "{compound.source_file}": ❌ result {round(diamag_contr, 2)} is not expected value: {sdf_test_compound.expected_diamag_total}'
         )
         raise e
