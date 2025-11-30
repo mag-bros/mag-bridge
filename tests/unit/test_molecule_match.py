932d2002d286cd00cd7e5660a9f4f755032ba458
Sevelantis <hamer223@o2.pl>
2025-11-30 03:57:47 +0100

    polish test

--- tests/unit/test_molecule_match.py
+++ tests/unit/test_molecule_match.py
@@ -3,43 +3,37 @@ from typing import Any
 
 import pytest
 from rdkit import Chem
+from rdkit.Chem import MolKey
 
 from src import MOLECULE_MATCH_SUBDIR, SDF_DIR
+from src.constants import COMMON_MOLECULES
 from src.core.compound import MBCompound
 from src.loader import SDFLoader
 
 
-@pytest.mark.parametrize("sdf_file", [p.name for p in SDF_DIR.joinpath(MOLECULE_MATCH_SUBDIR).glob("*.sdf")])
-def test_molecule_match(sdf_file: str) -> None:
-    """ Unit Test checking molecule substructure match utility done via SMILES format. """
+@pytest.mark.parametrize(
+    "common_mol",
+    enumerate(COMMON_MOLECULES.items()),
+    ids=lambda p: f"<test:{p[0]}> {p[1][0]}"
+)
+def test_molecule_match(common_mol: tuple[str, dict]) -> None:
+    """Unit Test checking molecule substructure match utility done via SMILES format."""
+    idx, (mol_key, mol_data) = common_mol
+    sdf_file = mol_data['sdf_file']
+    expected_smiles: set[str] = mol_data['SMILES']
+    
+    if sdf_file == '':
+        pytest.skip(f'test:{idx} SDF file NOT FOUND. Please create it for: "{mol_key}" --- {mol_data}')
+    
+    # Load all MBCompound instances from the given SDF file 
+    compound: MBCompound = SDFLoader.Load(sdf_file, subdir=MOLECULE_MATCH_SUBDIR)
 
-    # Load all MBCompound instances from the given SDF file
-    compound: MBCompound = SDFLoader.Load(sdf_file, subdir="molecule_match")
+    # List of SMILES representation for each molecule in given SDF compound
+    sdf_compound_smiles: set[str] = { mol.ToSmiles() for mol in compound.GetMols(to_rdkit=False) }
 
-    EXCEPTION = ["S2O32-.sdf"]
-
-    group = []
-    for mol in compound.GetMols(to_rdkit=False):
-        group.append(mol.ToSmiles())
-
-    # Test Results
-    try:
-        # trunk-ignore(bandit/B101)
-
-        # Exception (thiosulfate anion)
-        if compound.source_file in EXCEPTION:
-            print(
-                f'[INF] "{compound.source_file}": ⚠ Allowed {len(set(group))} canonical SMILES case: {set(group)}'
-            )
-
-        # Standard assertion
-        else:
-            assert len(set(group)) == 1
-            print(
-                f'[INF] "{compound.source_file}": ✅ One canonical SMILES created: {set(group)}'
-            )
-    except AssertionError as e:
-        print(
-            f'[ERR] "{compound.source_file}": ❌ {len(set(group))} canonical SMILES generated: {set(group)}'
-        )
-        raise e
+    # Actual test
+    # When two sets are compared, the order of their elements doesn't matter
+    test_condition: bool = sdf_compound_smiles == expected_smiles
+    
+    error_msg = f'[ERR]: mol_key: {mol_key}, mol_data: {mol_data}'
+    assert test_condition, error_msg
