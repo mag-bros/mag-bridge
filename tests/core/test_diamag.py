12d143c7bf547acb7abcc3d33d1454a6f9ca2019
Sevelantis <hamer223@o2.pl>
2025-12-04 23:42:42 +0100

    [tests] group diamag tests by subdir

--- tests/core/test_diamag.py
+++ tests/core/test_diamag.py
@@ -1,6 +1,10 @@
 import pytest
 
-from src import DIAMAG_COMPOUND_ATOMS_SUBDIR, DIAMAG_COMPOUND_MOLECULES_SUBDIR
+from src import (
+    DIAMAG_COMPOUND_ATOMS_SUBDIR,
+    DIAMAG_COMPOUND_CONSTITUTIVE_CORR_SUBDIR,
+    DIAMAG_COMPOUND_MOLECULES_SUBDIR,
+)
 from src.core.compound import MBCompound
 from src.loader import SDFLoader
 from tests.core.test_data import (
@@ -9,13 +13,10 @@ from tests.core.test_data import (
 )
 
 
-@pytest.mark.parametrize(
-    "test_case_params",
-    list(enumerate(CALC_DIAMAG_CONTR_TEST_CASES)),
-    ids=lambda p: f"<test:{p[0]}> {p[1].sdf_file}",
-)
-def test_calc_diamag_contr(
-    test_case_params: tuple[int, DiamagneticContributionTestSDF],
+def _run_diamag_contr(
+    subdir: str,
+    idx: int,
+    test_case: DiamagneticContributionTestSDF,
 ) -> None:
     """Internal Integration Test testing the whole diamag calcs process.
     Logical components:
@@ -23,16 +24,9 @@ def test_calc_diamag_contr(
         2. Calc Diamag Contr
         3. Compare calculated result to expected
     """
-    idx, test_case = test_case_params
-    # Load all MBMolecule instances from the given SDF file
-    compound: MBCompound = SDFLoader.Load(
-        test_case.sdf_file, subdir=DIAMAG_COMPOUND_ATOMS_SUBDIR
-    )
-
-    # Calc Diamag Contr of a Compound defined by a single SDF file
+    compound: MBCompound = SDFLoader.Load(test_case.sdf_file, subdir=subdir)
     diamag_contr: float = compound.CalcDiamagContr()
 
-    # Test Result
     try:
         assert round(diamag_contr, 2) == test_case.expected_contribution
         print(
@@ -43,3 +37,47 @@ def test_calc_diamag_contr(
             f'[ERR] "{compound.source_file}": ❌ result {round(diamag_contr, 2)} is not expected value: {test_case.expected_contribution}'
         )
         raise e
+
+
+def _params_for(subdir: str):
+    return pytest.mark.parametrize(
+        "diamag_contr_params",
+        list(enumerate(CALC_DIAMAG_CONTR_TEST_CASES[subdir])),
+        ids=lambda p: f"<test:{p[0]}> {p[1].sdf_file}",
+    )
+
+
+@_params_for(subdir=DIAMAG_COMPOUND_ATOMS_SUBDIR)
+def test_calc_diamag_contr_atoms(
+    diamag_contr_params: tuple[int, DiamagneticContributionTestSDF],
+) -> None:
+    idx, test_case = diamag_contr_params
+    _run_diamag_contr(
+        subdir=DIAMAG_COMPOUND_ATOMS_SUBDIR,
+        idx=idx,
+        test_case=test_case,
+    )
+
+
+@_params_for(subdir=DIAMAG_COMPOUND_MOLECULES_SUBDIR)
+def test_calc_diamag_contr_molecules(
+    diamag_contr_params: tuple[int, DiamagneticContributionTestSDF],
+) -> None:
+    idx, test_case = diamag_contr_params
+    _run_diamag_contr(
+        subdir=DIAMAG_COMPOUND_MOLECULES_SUBDIR,
+        idx=idx,
+        test_case=test_case,
+    )
+
+
+@_params_for(subdir=DIAMAG_COMPOUND_CONSTITUTIVE_CORR_SUBDIR)
+def test_calc_diamag_contr_constitutive_corr(
+    diamag_contr_params: tuple[int, DiamagneticContributionTestSDF],
+) -> None:
+    idx, test_case = diamag_contr_params
+    _run_diamag_contr(
+        subdir=DIAMAG_COMPOUND_CONSTITUTIVE_CORR_SUBDIR,
+        idx=idx,
+        test_case=test_case,
+    )
