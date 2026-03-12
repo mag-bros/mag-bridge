2c30b19ee890686e2fee9849bcc9fb95d19e6c2e
Sevelantis <hamer223@o2.pl>
2026-03-12 23:16:23 +0100

    alias

--- mcpmenago/scm_version.py
+++ mcpmenago/scm_version.py
@@ -3,5 +3,5 @@ import time
 
 
 def get_dynamic_version(version):
-    ver = f"py{sys.version_info.major}{sys.version_info.minor}"
-    return f"+{int(time.time())}.{py_version}"
+    package_version = f"py{sys.version_info.major}{sys.version_info.minor}"
+    return f"+{int(time.time())}.{package_version}"
